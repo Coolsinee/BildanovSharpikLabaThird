5c59b81af2cb25860627dde42d632dd9b598427a
RellTheSharpSlayer <emilbildanov@gmail.com>
2023-05-07 12:13:59 +0300

    Добавьте файлы проекта.

---
+++ BildanovSharpikLabaThird/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BildanovSharpikLabaThird
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем новый вектор с 3-мя элементами
+            Vector v1 = new Vector(3);
+
+            // Заполняем вектор случайными числами из диапазона [-1, 1)
+            v1.FillRandom(-1, 1);
+
+            // Выводим вектор в консоль
+            Console.WriteLine("Вектор v1 = " + v1.ToString());
+
+            // Создаем второй новый вектор с 3-мя элементами
+            Vector v2 = new Vector(3);
+
+            // Заполняем вектор случайными числами из диапазона [-1, 1)
+            v2.FillRandom(-1, 1);
+
+            // Выводим вектор в консоль
+            Console.WriteLine("Вектор v2 = " + v2.ToString());
+
+            // Вычислим сумму v1 и v2
+            Vector v3 = Vector.Add(v1, v2);
+
+            // Выводим сумму в консоль
+            Console.WriteLine("Вектор полученный суммированием значений векторов v1, v2 = " + v3.ToString());
+
+            // Вычислим скалярное произведение v1 и v2 
+            double dotProduct = Vector.Dot(v1, v2);
+
+            // Выводим скалярное произведение в консоль
+            Console.WriteLine("Скалярное произведение векторов v1, v2 = " + dotProduct);
+
+            // Проверим, равны ли v1 и v2
+            bool equal = Vector.Equals(v1, v2);
+
+            // Выводим результат в консоль
+            Console.WriteLine("Равны ли v1 и v2? " + equal);
+
+            // Получаем модуль v1
+            double modulus = v1.Modulus();
+
+            // Выводим модуль v1 в консоль
+            Console.WriteLine("Значение |v1| = " + modulus);
+
+            // Находим наибольший элемент v1
+            double largest = v1.Max();
+
+            // Выводим наибольший элемент v1
+            Console.WriteLine("Наибольший элемент v1 = " + largest);
+
+            // Получаем индекс наименьшего элемента из v1
+            int smallestIndex = v1.MinIndex();
+
+            // Выводим индекс в консоль
+            Console.WriteLine("Индекс наименьшего элемента из v1 = " + smallestIndex);
+
+            // Создаем новый вектор, состоящий только из положительных элементов v1
+            Vector v4 = v1.PositiveOnly();
+
+            // Выводим новый вектор в консоль
+            Console.WriteLine("Вектор v4(состоящий только из положительных элементов v1) = " + v4.ToString());
+
+            // Получаем элемент с индексом 1 из v1
+            double element = v1.Get(1);
+
+            // Выводим элемент в консоль
+            Console.WriteLine("Выводим элемент с индексом 1 из v1 = " + element);
+
+            // Изменяем элемент с индексом 1 в v1 на 0,5
+            v1.Set(1, 0.5);
+
+            // Выводим измененный вектор в консоль
+            Console.WriteLine("Измененный вектор v1(элемент с индексом 1 заменили на 0,5) = " + v1.ToString());
+
+            // Проверяем, все ли элементы v1 неотрицательны
+            bool nonNegative = v1.AllNonNegative();
+
+            // Выводим результат в консоль
+            Console.WriteLine("Все ли элементы v1 неотрицательны? " + nonNegative);
+
+            // Проверяем, отсортирован ли v1 в порядке возрастания
+            bool sorted = v1.IsSorted();
+
+            // Выводим результат в консоль
+            Console.WriteLine("Отсортирован ли v1 в порядке возрастания? " + sorted);
+
+            // Выполнить линейный поиск значения 0,5 в v1
+            int searchIndex = v1.LinearSearch(0.5);
+
+            // Выводим результат в консоль
+            Console.WriteLine("Индекс 0,5 в v1 = " + searchIndex);
+
+            // Пирамидальная сортировка
+            Vector v5 = new Vector(5);
+            v5.FillRandom(1.0, 5);
+            Console.WriteLine("Заданный вектор: " + v5);
+            v5.HeapSort();
+            Console.WriteLine("Пирамидально отсортированный вектор: " + v5);
+            // Сдвиг элементов вектора на заданное число позиций влево, освободившиеся элементы заполняются нулями
+            Vector v6 = new Vector(5);
+            v6.FillRandom(1.0, 5);
+            Console.WriteLine("Заданный вектор:");
+            Console.WriteLine(v6.ToString());
+            // Перемещаем элементы
+            v6.ShiftRight();
+            // output the shifted vector
+            Console.WriteLine("Сдвинутый вектор: ");
+            Console.WriteLine(v6.ToString());
+        }
+    }
+}
---
+++ BildanovSharpikLabaThird/Vector.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BildanovSharpikLabaThird
+{
+    class Vector
+    {
+        private double[] values; // Маасив
+
+        // Конструкторы
+        public Vector(int size)
+        {
+            values = new double[size];
+        }
+
+        public Vector(params double[] values)
+        {
+            this.values = values;
+        }
+
+        public Vector(Vector vector)
+        {
+            this.values = vector.values;
+        }
+
+        // Метод ввода с консоли
+        public static Vector Read()
+        {
+            Console.WriteLine("Введите векторные значения, разделенные пробелами:");
+            string input = Console.ReadLine();
+            double[] values = input.Split().Select(Double.Parse).ToArray();
+            return new Vector(values);
+        }
+
+        // ToString метод
+        public override string ToString()
+        {
+            return "[" + string.Join(", ", values) + "]";
+        }
+
+        // Экземплярные методы
+        public double Modulus()
+        {
+            return Math.Sqrt(values.Sum(x => x * x));
+        }
+
+        public double Max()
+        {
+            return values.Max();
+        }
+
+        public int MinIndex()
+        {
+            return Array.IndexOf(values, values.Min());
+        }
+
+        public Vector PositiveOnly()
+        {
+            return new Vector(values.Where(x => x > 0).ToArray());
+        }
+
+        // Статичные методы
+        public static Vector Add(Vector v1, Vector v2)
+        {
+            if (v1.values.Length != v2.values.Length)
+            {
+                throw new ArgumentException("Векторы должны иметь одинаковый размер");
+            }
+            return new Vector(v1.values.Zip(v2.values, (x, y) => x + y).ToArray());
+        }
+
+        public static double Dot(Vector v1, Vector v2)
+        {
+            if (v1.values.Length != v2.values.Length)
+            {
+                throw new ArgumentException("Векторы должны иметь одинаковый размер");
+            }
+            return v1.values.Zip(v2.values, (x, y) => x * y).Sum();
+        }
+
+        public static bool Equals(Vector v1, Vector v2)
+        {
+            if (v1.values.Length != v2.values.Length)
+            {
+                return false;
+            }
+            return v1.values.SequenceEqual(v2.values);
+        }
+
+        // Добавляем сеттеры, геттеры, методы для заполнения векторов и т.д
+        public double Get(int index)
+        {
+            return values[index];
+        }
+
+        public void Set(int index, double value)
+        {
+            values[index] = value;
+        }
+
+        public void FillRandom(double a, double b)
+        {
+            Random random = new Random();
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = random.NextDouble() * (b - a) + a;
+            }
+        }
+
+        public int LinearSearch(double value)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool IsSorted()
+        {
+            for (int i = 0; i < values.Length - 1; i++)
+            {
+                if (values[i] > values[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int BinarySearch(double value)
+        {
+            int left = 0;
+            int right = values.Length - 1;
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+                if (values[middle] == value)
+                {
+                    return middle;
+                }
+                else if (values[middle] < value)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+            return -1;
+        }
+
+        public void TimSort()
+        {
+            Array.Sort(values);
+        }
+
+        public void ShiftRight()
+        {
+            Console.WriteLine("Введите кол-во позиций для сдвига влево: ");
+            int positions = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < positions; i++)
+            {
+                double temp = values[0];
+                for (int j = 1; j < values.Length; j++)
+                {
+                    values[j - 1] = values[j];
+                }
+                values[values.Length - 1] = temp;
+            }
+            for (int i = 0; i < positions; i++)
+            {
+                values[i] = 0;
+            }
+        }
+
+        public bool AllNonNegative()
+        {
+            return values.All(x => x >= 0);
+        }
+
+        public void HeapSort()
+        {
+            int n = values.Length;
+
+            // Создаем максимальную кучу
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Heapify(n, i);
+            }
+
+            // Извлекаем элементы из кучи один за другим
+            for (int i = n - 1; i >= 0; i--)
+            {
+                double temp = values[0];
+                values[0] = values[i];
+                values[i] = temp;
+
+                Heapify(i, 0);
+            }
+        }
+        // Метод помощник для пирамидальной сортировки
+        private void Heapify(int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && values[left] > values[largest])
+            {
+                largest = left;
+            }
+
+            if (right < n && values[right] > values[largest])
+            {
+                largest = right;
+            }
+
+            if (largest != i)
+            {
+                double temp = values[i];
+                values[i] = values[largest];
+                values[largest] = temp;
+
+                Heapify(n, largest);
+            }
+        }
+    }
+}
